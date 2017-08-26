3f888b946ac59b5d2a74b3e957e2db893ec3fec7
yaw owusu <asirifiyaw@gmail.com>
2017-08-26 05:51:15 -0700

    Switching to Crystal collector Game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,47 +1,123 @@
 
-
 $(document).ready(function(){
-	//creating an object containg game characters
 
-	var character={
-		"obiWan":{
-			name:"Obi-Wan Kenobi",
-			health:120,
-			attackPower:8,
-			imageUrl: "assets/images/obi.jpg",
-			counterAttack:15
-		},
-		"lukeSky":{
-			name:"Luke Skywalker",
-			health:100,
-			attackPower:14,
-			imageUrl: "assets/images/luke.jpg",
-			counterAttack:5
-		},
-		"darthS":{
-			name:"Darth Sidious",
-			health:150,
-			attackPower:8,
-			imageUrl: "assets/images/darthSidious.jpg",
-			counterAttack:20
-		},
-		"darthM":{
-			name:"Darth Maul",
-			health:180,
-			attackPower:7,
-			imageUrl: "assets/images/darthSidious.jpg",
-			counterAttack:20
-		},
+//global variables	
+var wins=0;
+var losses=0;
+var totalScore=0;
+var computerNumber=0;
 
-	};
+var crystals = {
+	"red":{
+		imageUrl:"assets/images/red.png",
+		points:0
+	},
 
-	console.log(character.obiWan.name);
+	"yellow":{
+		imageUrl:"assets/images/yellow.png",
+		points:0
+	},
 
-	var myPlayer;
-	var myOpponent;
-	var opponents;
-	var attackResult;
-	var counterAttactResult = 1;
-	var killCount = 0;
+	"green":{
+		imageUrl:"assets/images/green.png",
+		points:0
+	},
+
+	"blue":{
+		imageUrl:"assets/images/blue.png",
+		points:0
+	}
+};
+
+console.log(crystals.red.points);
+
+//The init function will display all the crystals and set the "wins", "losses", and "Total Score" to 0
+//It will also display the randomly generated computer "Number"
+function init(){
+
+	$("#wins").html(wins);
+	$("#losses").html(losses);
+	$("#totalScore").html(totalScore);
+	
+	//Random number for the red crystal will be generated between 1 and 5
+	crystals.red.points = Math.round(Math.random() * (5 - 1)) + 1
+	$("#red").append("<img src= " + crystals.red.imageUrl + ">");
+
+	crystals.yellow.points = Math.round(Math.random() * (15 - 10)) + 10
+	$("#yellow").append("<img src= " + crystals.yellow.imageUrl + ">");
+
+	crystals.green.points = Math.round(Math.random() * (20 - 15)) + 15
+	$("#green").append("<img src= " + crystals.green.imageUrl + ">");
+
+	crystals.blue.points = Math.round(Math.random() * (2 - 1)) + 1
+	$("#blue").append("<img src= " + crystals.blue.imageUrl + ">");
+
+	
+	computerNumber = Math.round(Math.random() * (60 - 40)) + 40
+	$("#computerNumber").html(computerNumber);
+
+}
+init()
+
+$("#red").on("click", function(){
+
+	totalScore = totalScore + crystals.red.points;
+	compareNumbers();
+	$("#totalScore").html(totalScore);
+	
+})
+
+$("#yellow").on("click", function(){
+	totalScore = totalScore + crystals.yellow.points;
+	compareNumbers();
+	$("#totalScore").html(totalScore);
+})
+
+$("#green").on("click", function(){
+	totalScore = totalScore + crystals.green.points;
+	compareNumbers();
+	$("#totalScore").html(totalScore);
+})
+
+$("#blue").on("click", function(){
+	totalScore = totalScore + crystals.blue.points;
+	compareNumbers();
+	$("#totalScore").html(totalScore);	
+})
+
+//This function will compare the sum of the points with the computerNumber and to see if they're the same.
+//wins increase by 1 if they're the same else, losses increase by 1. Then then endGame function is called
+function compareNumbers(){
+
+	if (totalScore == computerNumber){
+		alert("Good Job, you added right");
+		wins++;
+		$("#wins").html(wins);
+		endGame();
+	}
+	if(totalScore > computerNumber){
+		alert("Oops!! your additions went over " + computerNumber + ".");	
+		losses++
+		$("#losses").html(losses);
+		endGame();
+	}
+}
+
+//this will reset the coputer-generated number as well as the values assigned to the crystals
+function endGame(){
+	totalScore=0;
+	$("#totalScore").html(totalScore);
+
+	computerNumber = Math.round(Math.random() * (60 - 40)) + 40;
+	$("#computerNumber").html(computerNumber);
+
+	crystals.red.points = Math.round(Math.random() * (5 - 1)) + 1;
+	crystals.yellow.points = Math.round(Math.random() * (15 - 10)) + 10;
+	crystals.green.points = Math.round(Math.random() * (20 - 15)) + 1;
+	crystals.blue.points = Math.round(Math.random() * (2 - 1)) + 1;
+}
+
+
 
 });
+
